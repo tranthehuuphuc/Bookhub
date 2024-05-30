70c1335562fefccdcc336f2729e46f1a5920f042
hira <22521709@gm.uit.edu.vn>
2024-05-30 14:36:27 +0700

    minor update on log in

--- signin/php/login.php
+++ signin/php/login.php
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($result["role"] == "admin") {
             header("Location: ../../admin/admin.php");
         } else {
-            header("Location: ../../Account/AccountSettings.html");
+            header("Location: ../../Account/profile.html");
         }
 
         $pdo = null;
