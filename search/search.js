00c0cdce19825826dc7adc9819660083492ee552
tuyen2201 <22521630@gm.uit.edu.vn>
2024-05-31 14:54:09 +0700

    update filter search page
    update filter search page

--- search/search.js
+++ search/search.js
@@ -1,102 +1,31 @@
-let products = {
-    data: [
-        {
-            productName: 'Điều kỳ diệu của tiệm tạp hóa Namiya',
-            category: 'Sách',
-            price: '100.000',
-            image: '../assets/biasachvd.jpg',
-        },
-        {
-            productName: 'Sách 2',
-            category: 'Sách',
-            price: '100.000',
-            image: '../assets/biasachvd1.jpg',
-        },
-        {
-            productName: 'Sách 2',
-            category: 'Sách',
-            price: '100.000',
-            image: '../assets/biasachvd1.jpg',
-        },
-        {
-            productName: 'Sách 2',
-            category: 'Sách',
-            price: '100.000',
-            image: '../assets/biasachvd1.jpg',
-        },
-        {
-            productName: 'Sách 2',
-            category: 'Sách',
-            price: '100.000',
-            image: '../assets/biasachvd1.jpg',
-        },
-        {
-            productName: 'Sách 2',
-            category: 'Sách',
-            price: '100.000',
-            image: '../assets/biasachvd1.jpg',
-        },
-        {
-            productName: 'Sách 2',
-            category: 'Thể loại',
-            price: '100.000',
-            image: '../assets/biasachvd1.jpg',
-        },
-        {
-            productName: 'Sách 2',
-            category: 'Thể loại',
-            price: '100.000',
-            image: '../assets/biasachvd1.jpg',
-        },
-        {
-            productName: 'Sách 2',
-            category: 'Tác giả',
-            price: '100.000',
-            image: '../assets/biasachvd1.jpg',
-        },
-        {
-            productName: 'Sách 3',
-            category: 'Tác giả',
-            price: '100.000',
-            image: '../assets/biasachvd.jpg',
-        },
-        {
-            productName: 'Sách 4',
-            category: 'Thảo luận',
-            price: '100.000',
-            image: '../assets/biasachvd.jpg',
-        },
-    ],
-};
-
-for (let i of products.data) {
-    //Create Card
-    let card = document.createElement("div");
-    //Card should have category and should stay hidden initially
-    card.classList.add("card", i.category.toLowerCase().replace(/\s+/g, '_'), "hide");
-    //image div
-    let imgContainer = document.createElement("div");
-    imgContainer.classList.add("image-container");
-    //img tag
-    let image = document.createElement("img");
-    image.setAttribute("src", i.image);
-    imgContainer.appendChild(image);
-    card.appendChild(imgContainer);
-    //container
-    let container = document.createElement("div");
-    container.classList.add("container");
-    //product name
-    let name = document.createElement("h4");
-    name.classList.add("product-name");
-    name.innerText = i.productName.toUpperCase();
-    container.appendChild(name);
-    //price
-    let price = document.createElement("h3");
-    price.innerText = i.price + 'đ';
-    container.appendChild(price);
-    card.appendChild(container);
-    document.getElementById("products").appendChild(card);
-  }
+// for (let i of products.data) {
+//     //Create Card
+//     let card = document.createElement("div");
+//     //Card should have category and should stay hidden initially
+//     card.classList.add("card", i.category.toLowerCase().replace(/\s+/g, '_'), "hide");
+//     //image div
+//     let imgContainer = document.createElement("div");
+//     imgContainer.classList.add("image-container");
+//     //img tag
+//     let image = document.createElement("img");
+//     image.setAttribute("src", i.image);
+//     imgContainer.appendChild(image);
+//     card.appendChild(imgContainer);
+//     //container
+//     let container = document.createElement("div");
+//     container.classList.add("container");
+//     //product name
+//     let name = document.createElement("h4");
+//     name.classList.add("product-name");
+//     name.innerText = i.productName.toUpperCase();
+//     container.appendChild(name);
+//     //price
+//     let price = document.createElement("h3");
+//     price.innerText = i.price + 'đ';
+//     container.appendChild(price);
+//     card.appendChild(container);
+//     document.getElementById("products").appendChild(card);
+//   }
 
   //parameter passed from button (Parameter same as category)
   function filterProduct(value) {
@@ -131,28 +60,28 @@ for (let i of products.data) {
     });
   }
 
-  function searchProduct() {
-    let searchInput = document.getElementById("search-input").value;
-    let elements = document.querySelectorAll(".product-name");
-    let cards = document.querySelectorAll(".card");
+  // function searchProduct() {
+  //   let searchInput = document.getElementById("search-input").value;
+  //   let elements = document.querySelectorAll(".product-name");
+  //   let cards = document.querySelectorAll(".card");
     
-    elements.forEach((element, index) => {
-      if (element.innerText.includes(searchInput.toUpperCase())) {
-        cards[index].classList.remove("hide");
-      } else {
-        cards[index].classList.add("hide");
-      }
-    });
-  }
+  //   elements.forEach((element, index) => {
+  //     if (element.innerText.includes(searchInput.toUpperCase())) {
+  //       cards[index].classList.remove("hide");
+  //     } else {
+  //       cards[index].classList.add("hide");
+  //     }
+  //   });
+  // }
 
-  document.getElementById("search-input").addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        searchProduct();
-    }
-    });
-  document.getElementById("search").addEventListener("click", searchProduct);
+  // document.getElementById("search-input").addEventListener("keypress", function(event) {
+  //   if (event.key === "Enter") {
+  //       searchProduct();
+  //   }
+  //   });
+  // document.getElementById("search").addEventListener("click", searchProduct);
 
-  //Initially display all products
-  window.onload = () => {
-    filterProduct("all");
-  };
+  // //Initially display all products
+  // window.onload = () => {
+  //   filterProduct("all");
+  // };
