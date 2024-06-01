fe2b8594a1363c950abac85f164dc59240d9ffaa
tuyen2201 <22521630@gm.uit.edu.vn>
2024-06-01 10:29:42 +0700

    update search page and add condition for search data
    update search page and add condition for search data

--- search/php/search_model.php
+++ search/php/search_model.php
@@ -3,9 +3,15 @@
 declare(strict_types=1);
 
 function get_books(object $pdo, string $title): array {
-    $query = "SELECT * FROM books WHERE title = :title";
+    // $query = "SELECT * FROM books WHERE title = :title";
+    // $stmt = $pdo->prepare($query);
+    // $stmt->bindParam(":title", $title);
+    // $stmt->execute();
+
+    $query = "SELECT * FROM books WHERE title LIKE :title";
     $stmt = $pdo->prepare($query);
-    $stmt->bindParam(":title", $title);
+    $searchTitle = '%' . $title . '%'; // Adding wildcards for partial search
+    $stmt->bindParam(":title", $searchTitle, PDO::PARAM_STR);
     $stmt->execute();
 
     // $result = $stmt->fetch(PDO::FETCH_ASSOC);
